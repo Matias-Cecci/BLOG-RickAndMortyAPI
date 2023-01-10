eeeb84832f0d18eb2578aba38d594fa7ddbc250a
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-01-10 13:19:44 +0000

    Finalizado Details

--- src/js/views/details.js
+++ src/js/views/details.js
@@ -18,12 +18,30 @@ export const Details = () => {
 	}
 
 	return (
-		<div className="container">
-			<h1>Details {params.type}</h1>
-			<p>Name: {itemDetails.name}</p>
-			{Object.keys(itemDetails).map((x,i)=> {
-				if(typeof itemDetails[x] != "object") {return (<p key={i}>{x}: {itemDetails[x]}</p>);}
-			})}
+		<div className="container d-flex justify-content-center">
+			<div className="card mb-3" style={{maxWidth: "540px"}}>
+				<div className="row g-0">
+					{itemDetails.image ? <div className="col-md-4">
+						<img src={itemDetails.image} className="img-fluid rounded-start" alt="..."/>
+					</div>  : null}
+					
+					<div className="col-md-8">
+						<div className="card-body">
+							<h2><b>{params.type.toUpperCase()}</b></h2>
+							<h4 className="card-title"><b>Name: </b>{itemDetails.name}</h4>
+							<div className="card-text">	
+								{Object.keys(itemDetails).map((x,i)=> {
+									if(typeof itemDetails[x] != "object") {
+										if(itemDetails[x] !== itemDetails.id & itemDetails[x] !== itemDetails.name){
+										return (<p key={i}><b>{x}:</b> {itemDetails[x]}</p>)
+									}		
+								}	
+								})}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
